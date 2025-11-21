bf6958dc9269a009ffba202feabbbda3a23a7464
Mikhail Yurikov <yurikov280692@gmail.com>
2025-11-22 00:15:43 +1000

    добавил magic

--- src/builder_and_singelton/task3/Product.java
+++ src/builder_and_singelton/task3/Product.java
@@ -3,13 +3,14 @@ package builder_and_singelton.task3;
 import java.util.List;
 
 public class Product {
+    private static final int SHOP_COEF = 2;
     protected int price;
     protected String manufacturer;
     protected String model;
 
 
     public Product(int price, String manufacturer, String model) {
-        this.price = price;
+        this.price = price * SHOP_COEF;
         this.manufacturer = manufacturer;
         this.model = model;
     }
